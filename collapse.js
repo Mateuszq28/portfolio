88673b4b349b3eb1eb2aaa03a989e013409c0ebb
Mateusz Miler Pikus <mateuszq28@gmail.com>
2024-06-21 21:54:37 +0200

    animation works! gone

--- collapse.js
+++ collapse.js
@@ -14,12 +14,27 @@
 //     });
 // }
 
+var wait_time = 500;
+
+function change_class(elem, from_c, to_c) {
+  elem.classList.remove(from_c);
+  elem.classList.add(to_c);
+}
+
+function content_roller(cont) {
+  if (cont.style.maxHeight) {
+    cont.style.maxHeight = null;
+  } else {
+    cont.style.maxHeight = cont.scrollHeight + "px";
+  }
+}
 
 
 // with animation
 var coll_ = document.getElementsByClassName("red_mor");
 var i;
-
+// main loop to add func to all instances of the class "red_mor"
+// the class can be changed in future operations, but this func will be assign to element forever
 for (i = 0; i < coll_.length; i++) {
   coll_[i].addEventListener("click", function() {
     var coll = this
@@ -27,24 +42,22 @@ for (i = 0; i < coll_.length; i++) {
     var content = hid_but.nextElementSibling;
     // var hid_but2 = content.nextElementSibling;
 
-    coll.classList.remove("red_mor");
-    coll.classList.add("red_mor_hidden");
-    hid_but.classList.remove("hid_but_hidden");
-    hid_but.classList.add("hid_but");
-    // hid_but2.classList.toggle("hid_but2");
+    change_class(coll, "red_mor", "red_mor_hidden");
+    change_class(hid_but, "hid_but_gone", "hid_but_hidden");
+    change_class(hid_but, "hid_but_hidden", "hid_but");
     
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    setTimeout(function() {
+      change_class(coll, "red_mor_hidden", "red_mor_gone")
+    }, wait_time);
+    
+    content_roller(content);
   });
 }
 
 
-var hid_but_ = document.getElementsByClassName("hid_but_hidden");
+var hid_but_ = document.getElementsByClassName("hid_but_gone");
 var i;
-
+// main loop to add func to all instances of the class "hid_but_gone"
 for (i = 0; i < hid_but_.length; i++) {
   hid_but_[i].addEventListener("click", function() {
     var coll = this.parentNode.firstElementChild;
@@ -52,24 +65,22 @@ for (i = 0; i < hid_but_.length; i++) {
     var content = hid_but.nextElementSibling;
     // var hid_but2 = content.nextElementSibling;
 
-    coll.classList.remove("red_mor_hidden");
-    coll.classList.add("red_mor");
-    hid_but.classList.remove("hid_but");
-    hid_but.classList.add("hid_but_hidden");
-    // hid_but2.classList.toggle("hid_but2_hidden");
+    change_class(coll, "red_mor_gone", "red_mor_hidden");
+    change_class(coll, "red_mor_hidden", "red_mor");
+    change_class(hid_but, "hid_but", "hid_but_hidden");
 
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    setTimeout(function() {
+      change_class(hid_but, "hid_but_hidden", "hid_but_gone")
+    }, wait_time);
+
+    content_roller(content);
   });
 }
 
 
 var hid_but2_ = document.getElementsByClassName("hid_but2");
 var i;
-
+// main loop to add func to all instances of the class "hid_but2"
 for (i = 0; i < hid_but2_.length; i++) {
   hid_but2_[i].addEventListener("click", function() {
     var hid_but2 = this;
@@ -77,16 +88,14 @@ for (i = 0; i < hid_but2_.length; i++) {
     var hid_but = coll.nextElementSibling;
     var content = hid_but.nextElementSibling;
 
-    coll.classList.remove("red_mor_hidden");
-    coll.classList.add("red_mor");
-    hid_but.classList.remove("hid_but");
-    hid_but.classList.add("hid_but_hidden");
-    // hid_but2.classList.toggle("hid_but2_hidden");
+    change_class(coll, "red_mor_gone", "red_mor_hidden");
+    change_class(coll, "red_mor_hidden", "red_mor");
+    change_class(hid_but, "hid_but", "hid_but_hidden");
 
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    setTimeout(function() {
+      change_class(hid_but, "hid_but_hidden", "hid_but_gone")
+    }, wait_time);
+
+    content_roller(content);
   });
 }
